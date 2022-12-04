b870c9c2b0f54ed5c3a9f932b5d46b4cc9285e6d
KUNG3N <abdumah2021fr@gmail.com>
2022-12-04 20:50:40 +0100

    bla bla

---
+++ js/first.js
@@ -0,0 +1,18 @@
+// let myname = "king";
+// myname = "abdu";
+
+// let urname = prompt("Whats ur name bitch?");
+// alert("Hello " + urname);
+// alert("my name is " + myname + " ,waddup");
+
+
+// let message = "whats up ";
+// message.length;
+// let bitchName = "bitch";
+// alert (message + bitchName);
+
+// character counter js code to inplement in character wounder website
+// let bars = prompt("write what u want fam");
+// let textCount = bars.length;
+// let whatsLeft = (280 - textCount);
+// alert (`u have used ${textCount} characters out of 280, u only have ${whatsLeft} left.`);
